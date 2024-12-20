8f562359e27bea1aae6fc926d8e9527e7c8c72b3
Makenna Worley <39921273+MakennaWorley@users.noreply.github.com>
2024-12-20 16:37:14 -0700

    Day 19 and 20

---
+++ 2024/day19.py
@@ -0,0 +1,30 @@
+# day 19
+def ways(words, target):
+    if target in dict:
+        return dict[target]
+    ans = 0
+    if not target:
+        ans = 1
+    for word in words:
+        if target.startswith(word):
+            ans += ways(words, target[len(word):])
+    dict[target] = ans
+    return ans
+
+
+count = 0
+count2 = 0
+
+text = open('inputs/day19_input.txt').read().strip()
+words, targets = text.split('\n\n')
+words = words.split(', ')
+
+dict = {}
+for target in targets.split('\n'):
+    target_ways = ways(words, target)
+    if target_ways > 0:
+        count += 1
+    count2 += target_ways
+
+print(count)
+print(count2)
---
+++ 2024/day20.py
@@ -0,0 +1,75 @@
+# day 20
+from collections import deque
+
+DIRS = [(-1,0),(0,1),(1,0),(0,-1)] # up right down left
+
+ans = 0
+D = open('inputs/day20_input.txt').read().strip()
+
+graph = D.split('\n')
+row = len(graph)
+column = len(graph[0])
+graph = [[graph[r][c] for c in range(column)] for r in range(row)]
+
+for r in range(row):
+    for c in range(column):
+        if graph[r][c] == 'S':
+            sr,sc = r,c
+        if graph[r][c] == 'E':
+            er,ec = r,c
+
+DIST = {}
+queue = deque([(0,er,ec)])
+while queue:
+    d,r,c = queue.popleft()
+    if (r,c) in DIST:
+        continue
+    DIST[(r,c)] = d
+    for dr,dc in DIRS:
+        rr,cc = r+dr, c+dc
+        if 0<=rr<row and 0<=cc<column and graph[rr][cc]!='#':
+            queue.append((d+1,rr,cc))
+
+def find_cheat(d0, columnHEAT_TIME):
+    ans = set()
+    queue = deque([(0,None,None,None,sr,sc)])
+    SEEN = set()
+    SAVE = 100
+    while queue:
+        d,cheat_start,cheat_end,cheat_time,r,c = queue.popleft()
+        assert cheat_end is None
+        if d>=d0-SAVE:
+            continue
+        if graph[r][c] == 'E':
+            if cheat_end is None:
+                cheat_end = (r,c)
+            if d<=d0-SAVE and (cheat_start,cheat_end) not in ans:
+                ans.add((cheat_start, cheat_end))
+        if (r,c,cheat_start,cheat_end,cheat_time) in SEEN:
+            continue
+        SEEN.add((r,c,cheat_start,cheat_end,cheat_time))
+
+        if cheat_start is None:
+            assert graph[r][c] != '#'
+            queue.append((d,(r,c),None,columnHEAT_TIME,r,c))
+        if cheat_time is not None and graph[r][c]!='#':
+            assert graph[r][c] in ['.', 'S', 'E']
+            if DIST[(r,c)] <= d0-100-d:
+                ans.add((cheat_start, (r,c)))
+        if cheat_time == 0:
+            continue
+        else:
+            for dr,dc in DIRS:
+                rr,cc = r+dr, c+dc
+                if cheat_time is not None:
+                    assert cheat_time > 0
+                    if 0<=rr<row and 0<=cc<column:
+                        queue.append((d+1,cheat_start,None,cheat_time-1,rr,cc))
+                else:
+                    if 0<=rr<row and 0<=cc<column and graph[rr][cc]!='#':
+                        queue.append((d+1,cheat_start,cheat_end,cheat_time,rr,cc))
+    return len(ans)
+
+d0 = DIST[(sr,sc)]
+print(find_cheat(d0, 2))
+print(find_cheat(d0, 20))
