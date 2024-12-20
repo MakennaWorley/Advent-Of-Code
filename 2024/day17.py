4d92000c8fc0305808a5ea4a144c51af6cd82970
Makenna Worley <39921273+MakennaWorley@users.noreply.github.com>
2024-12-20 16:51:36 -0700

    Day 17 part 2

--- 2024/day17.py
+++ 2024/day17.py
@@ -17,7 +17,7 @@ def getCombo(x, A=None, B=None, C=None):
     return -1
 
 
-def run(Ast, part2):
+def run(Ast):
     A = Ast
     B = 0
     C = 0
@@ -25,67 +25,60 @@ def run(Ast, part2):
     out = []
 
     while True:
-        if ip >= len(program):
-            return out
-        cmd = program[ip]
-        op = program[ip + 1]
-        combo = getCombo(op, A, B, C)
+        if ip < 0 or ip >= len(program):
+            break
+        opcode = program[ip]
+        operand = program[ip + 1]
 
-        if cmd == 0:
-            A = A // 2 ** combo
-            ip += 2
-
-        elif cmd == 1:
-            B = B ^ op
-            ip += 2
-
-        elif cmd == 2:
-            B = combo % 8
-            ip += 2
-
-        elif cmd == 3:
+        if opcode == 0:
+            A = A // (2 ** getCombo(operand, A, B, C))
+        elif opcode == 1:
+            B = B ^ operand
+        elif opcode == 2:
+            B = getCombo(operand, A, B, C) % 8
+        elif opcode == 3:
             if A != 0:
-                ip = op
-
-            else:
-                ip += 2
-
-        elif cmd == 4:
+                ip = operand - 2
+        elif opcode == 4:
             B = B ^ C
-            ip += 2
+        elif opcode == 5:
+            out.append(getCombo(operand, A, B, C) % 8)
+        elif opcode == 6:
+            B = A // (2 ** getCombo(operand, A, B, C))
+        elif opcode == 7:
+            C = A // (2 ** getCombo(operand, A, B, C))
 
-        elif cmd == 5:
-            out.append(int(combo % 8))
-            ip += 2
+        ip += 2
 
-        elif cmd == 6:
-            B = A // 2 ** combo
-            ip += 2
-
-        elif cmd == 7:
-            C = A // 2 ** combo
-            ip += 2
+    return out
 
 
-def find_lowest_A(program):
-    Ast = 1
-    while True:
-        A = Ast * 8
-        out = run(A, True)
-        if out == program:
-            return Ast
-        Ast += 1
+def find_quine(Ast):
+    out = []
+    matched = program[-1:]
+    Ast = 8 ** 15
+    power = 14
+
+    while out != program:
+        Ast += 8 ** power
+        out = run(Ast)
+        if out[-len(matched):] == matched:
+            power = max(0, power - 1)
+            matched = program[-(len(matched) + 1):]
+
+    return Ast
 
 
 ans = 0
 D = open('inputs/day17_input.txt').read().strip()
+matches = [int(x) for x in re.findall(r"\d+", D)]
 
 regs, program = D.split('\n\n')
 A,B,C = ints(regs)
 program = program.split(':')[1].strip().split(',')
 program = [int(x) for x in program]
 
-part1 = run(A, False)
-print(','.join([str(x) for x in part1]))
+print(",".join([str(x) for x in run(A)]))
 
-print(find_lowest_A(program))
+Ast = find_quine(A)
+print(Ast)
